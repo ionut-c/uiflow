f7a49df120a72c7d8ee69e73c4ade3ad510fa2ca
Radu Baciu <radu.baciu@techromix.com>
2015-09-03 11:00:58 +0300

    styling header

--- plps/js/main.js
+++ plps/js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+    $(".language").click(function() {
+    	$(".select-language").show();
+    });
 
+    $("#search").click(function() {
+    	$(".search-form").toggle();
+    	$( ".search-form input" ).focus();
+    });
+    $(".close-search").click(function() {
+    	$(".search-form").hide();
+    });
+});
